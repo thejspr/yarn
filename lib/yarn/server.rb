ba36e7bff0d1bf66f939613f58a4f671b8dc9898
Jesper Kjeldgaard <jkjeldgaard@gmail.com>
2011-09-30 00:40:15 +0100

    Thread counter and debug

--- lib/yarn/server.rb
+++ lib/yarn/server.rb
@@ -24,19 +24,19 @@ module Yarn
     end
 
     def create_listener
-      threads = []
-      counter = 0
+      @threads = []
+      @counter = 0
       @socket_listener = Thread.new do
         loop do
           begin
             # waits here until a requests comes in
             session = @socket.accept
-            threads << Thread.new { 
+            @threads << Thread.new { 
               (@app ? RackHandler.new(@app) : RequestHandler.new).run session 
             }
-            counter += 1
-            if count % 10 == 0
-              debug "Thread count: #{threads.size}"
+            @counter += 1
+            if @counter % 10 == 0
+              debug "Thread count: #{@threads.size}"
             end
           rescue Exception => e
             session.close
@@ -50,7 +50,6 @@ module Yarn
     def start
       log "Yarn started and accepting requests on #{@host}:#{@port}"
       begin
-        @worker_pool.listen!
         @socket_listener.join
       rescue Interrupt => e
         log "Caught interrupt, stopping..."
@@ -65,6 +64,8 @@ module Yarn
       @socket_listener.kill if @socket_listener
 
       log "Server stopped"
+      debug "Served #{@counter} requests"
+      debug "#{@threads.collect { |t| t.alive? }.size} live threads"
     end
   end
 end
