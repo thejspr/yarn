a732942f34361e94c67df396f1d9b9c81307913c
Jesper Kjeldgaard <jkjeldgaard@gmail.com>
2011-10-02 05:29:05 +0100

    Load rack apps in ruby instead of rackup

--- features/rack.feature
+++ features/rack.feature
@@ -5,13 +5,11 @@ Feature: Implement rack interface
   In order to serve rack applications
 
   Scenario: Serve a one-file rack application
-    Given I have a rack application "config.ru"
-    And the rack application "config.ru" is running
+    Given the rack test app is running
     When I go to "/"
     Then the response should contain "Rack works"
 
   Scenario: Serve a rails application
-    Given I have a rack application "rails_test/config.ru"
-    And the rack application "rails_test/config.ru" is running
+    Given the rails test app is running
     When I go to ""
     Then the response should contain "Yarn Test Blog"
--- features/step_definitions/rack_steps.rb
+++ features/step_definitions/rack_steps.rb
@@ -1,10 +1,20 @@
+require 'rack'
+
 Given /^I have a rack application "([^"]*)"$/ do |app|
    testfile_exists?(app).should be_true 
 end
 
-Given /^the rack application "([^"]*)" is running$/ do |app|
-  system("rackup -s Yarn test_objects/#{app} &")
-  sleep 3
-  res = `ps aux | grep [r]uby | grep [r]ackup`
-  @rack_server = res.split[1].to_i
+Given /^the rack test app is running$/ do
+  config_file = File.read("test_objects/config.ru")
+  rack_application = eval("Rack::Builder.new { #{config_file} }")
+  start_server(3000,rack_application)
+end
+
+Given /^the rails test app is running$/ do
+  current_dir = Dir.pwd
+  Dir.chdir("test_objects/rails_test")
+  config_file = File.read("config.ru")
+  rack_application = eval("Rack::Builder.new { #{config_file} }")
+  start_server(3000,rack_application)
+  Dir.chdir(current_dir)
 end
