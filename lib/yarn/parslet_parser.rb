0b0c292ab1bf409cd9cc16b3c6bb0ff00e32f3e3
Jesper Kjeldgaard <jkjeldgaard@gmail.com>
2011-10-05 04:46:03 +0100

    Refactor incorrect usage of repeat in parser

--- lib/yarn/parslet_parser.rb
+++ lib/yarn/parslet_parser.rb
@@ -1,8 +1,6 @@
 require 'rubygems'
 require 'parslet'
 
-module Parslet::Atoms::DSL; def once; repeat(1); end; end
-
 module Yarn
   class ParsletParser < Parslet::Parser
     
@@ -15,9 +13,9 @@ module Yarn
     rule(:spaces) { match('\s+') }
 
     # header rules
-    rule(:header_value) { match['^\r\n'].once }
+    rule(:header_value) { match['^\r\n'].repeat(1) }
 
-    rule(:header_name) { match['a-zA-Z\-'].once }
+    rule(:header_name) { match['a-zA-Z\-'].repeat(1) }
 
     rule(:header) do 
       header_name.as(:name) >> 
@@ -28,7 +26,7 @@ module Yarn
     end
 
     # request-line rules
-    rule(:http_version) { match['HTTP\/\d\.\d'].once }
+    rule(:http_version) { match['HTTP\/\d\.\d'].repeat(1) }
 
     rule(:query) do
       match['\S+'].repeat(1)
@@ -37,12 +35,12 @@ module Yarn
     rule(:path) do 
       match['^\?'].repeat(1).as(:path) >> 
       str("?") >> 
-      query.as(:query) | match['^\s'].once.as(:path)
+      query.as(:query) | match['^\s'].repeat(1).as(:path)
     end
 
     rule(:port) { match['\d+'].repeat(1) }
 
-    rule(:host) { match['^\/:'].once }
+    rule(:host) { match['^\/:'].repeat(1) }
 
     rule(:absolute_uri) do
       str("http://") >> 
@@ -56,7 +54,7 @@ module Yarn
 
     rule(:spaces) { match('\s+') }
 
-    rule(:method) { match['OPTIONS|GET|HEAD|POST|PUT|DELETE|TRACE|CONNECT'].once }
+    rule(:method) { match['OPTIONS|GET|HEAD|POST|PUT|DELETE|TRACE|CONNECT'].repeat(1) }
 
     # RFC2616: Method SP Request-URI SP HTTP-Version CRLF
     rule(:request_line) do 
@@ -69,7 +67,7 @@ module Yarn
     end
      
     # body rule
-    rule(:body) { match['\S'].once }
+    rule(:body) { match['\S'].repeat(1) }
 
     # RFC2616: Request-Line *(( header ) CRLF) CRLF [ message-body ]
     rule(:request) do 
